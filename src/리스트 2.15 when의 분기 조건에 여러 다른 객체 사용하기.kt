42e1cd52a8cfb4ba9c2e006447ec72f1d896372b
bnitech <bnitech88@gmail.com>
2020-08-19 02:31:33 +0900

    리스트 2.15 when의 분기 조건에 여러 다른 객체 사용하기

---
+++ src/리스트 2.15 when의 분기 조건에 여러 다른 객체 사용하기.kt
@@ -0,0 +1,9 @@
+import Color.*
+
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)) {
+        setOf(RED, YELLOW) -> ORANGE
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty color")
+    }
