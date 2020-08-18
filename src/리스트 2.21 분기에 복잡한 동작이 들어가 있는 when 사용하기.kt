cc4275bef539757caf259d418836fcf55985d1ee
bnitech <bnitech88@gmail.com>
2020-08-19 02:33:15 +0900

    리스트 2.21 분기에 복잡한 동작이 들어가 있는 when 사용하기

---
+++ src/리스트 2.21 분기에 복잡한 동작이 들어가 있는 when 사용하기.kt
@@ -0,0 +1,14 @@
+fun evalWithLogging(e: Expr): Int =
+    when (e) {
+        is Num -> {
+            println("num: ${e.value}")
+            e.value
+        }
+        is Sum -> {
+            val left = evalWithLogging(e.left)
+            val right = evalWithLogging(e.right)
+            println("sum: $left + $right")
+            left + right
+        }
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
