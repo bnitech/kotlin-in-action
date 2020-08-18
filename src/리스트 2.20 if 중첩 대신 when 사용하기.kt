f73d93f33b3731221f5e7d4a84fc68ddc3ffadd2
bnitech <bnitech88@gmail.com>
2020-08-19 02:33:01 +0900

    리스트 2.20 if 중첩 대신 when 사용하기

---
+++ src/리스트 2.20 if 중첩 대신 when 사용하기.kt
@@ -0,0 +1,6 @@
+fun eval(e: Expr): Int =
+    when (e) {
+        is Num -> e.value
+        is Sum -> eval(e.right) + eval(e.left)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
