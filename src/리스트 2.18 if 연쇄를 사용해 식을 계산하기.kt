49f157eadfff42b94a82fd3688c42d0d1fd2084c
bnitech <bnitech88@gmail.com>
2020-08-19 02:32:28 +0900

    리스트 2.18 if 연쇄를 사용해 식을 계산하기

---
+++ src/리스트 2.18 if 연쇄를 사용해 식을 계산하기.kt
@@ -0,0 +1,10 @@
+fun eval(e: Expr): Int {
+    if (e is Num) {
+        val n = e as Num
+        return n.value
+    }
+    if (e is Sum) {
+        return eval(e.right) + eval(e.left)
+    }
+    throw IllegalArgumentException("Unknown expression")
+}
