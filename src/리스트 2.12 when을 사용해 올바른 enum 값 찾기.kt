33391a1d2a0fe962472df0c76ed11d8bc4a8272f
bnitech <bnitech88@gmail.com>
2020-08-19 02:30:57 +0900

    리스트 2.12 when을 사용해 올바른 enum 값 찾기

---
+++ src/리스트 2.12 when을 사용해 올바른 enum 값 찾기.kt
@@ -0,0 +1,11 @@
+fun getMnemonic(color: Color) {
+    when (color) {
+        Color.RED -> "Richard"
+        Color.ORANGE -> "Of"
+        Color.YELLOW -> "York"
+        Color.GREEN -> "Gave"
+        Color.BLUE -> "Battle"
+        Color.INDIGO -> "In"
+        Color.VIOLET -> "Vain"
+    }
+}
