c62fcaab3d06e3e90ec85beb2bf7a283a5bcf36b
bnitech <bnitech88@gmail.com>
2020-08-23 16:53:42 +0900

    리스트 2.27 자바와 마찬가지로 try 사용하기

---
+++ src/리스트 2.27 자바와 마찬가지로 try 사용하기.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+
+fun readNumber(reader: BufferedReader): Int? {
+    try {
+        val line = reader.readLine()
+        return Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        return null
+    } finally {
+        reader.close()
+    }
+}
