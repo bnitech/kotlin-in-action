2d325d642fd73cbdf2041697b85ac4f1b2a21737
bnitech <bnitech88@gmail.com>
2020-08-25 02:22:49 +0900

    리스트 3.12 로컬 함수를 사용해 코드 중복 줄이기

--- src/리스트 3.11 코드 중복을 보여주는 예제.kt
+++ src/리스트 3.11 코드 중복을 보여주는 예제.kt
@@ -1,3 +1,4 @@
+/*
 class User(val id: Int, val name: String, val address: String)
 
 fun saveUser(user: User) {
@@ -9,4 +10,4 @@ fun saveUser(user: User) {
         throw java.lang.IllegalArgumentException("Can't save user ${user.id}: empty Address")
     }
     // user를 데이터베이스에 저장한다.
-}
+}*/
