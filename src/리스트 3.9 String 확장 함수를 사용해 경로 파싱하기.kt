7473b12533342cd0909811f7f27c6224d9e23513
bnitech <bnitech88@gmail.com>
2020-08-25 02:21:24 +0900

    리스트 3.9 String 확장 함수를 사용해 경로 파싱하기

---
+++ src/리스트 3.9 String 확장 함수를 사용해 경로 파싱하기.kt
@@ -0,0 +1,7 @@
+fun parsePath(path: String) {
+    val directory = path.substringBeforeLast("/")
+    val fullName = path.substringAfterLast("/")
+    val fileName = fullName.substringBeforeLast(".")
+    val extension = fullName.substringAfterLast(".")
+    println("Dir: $directory, name: $fileName, ext: $extension")
+}
