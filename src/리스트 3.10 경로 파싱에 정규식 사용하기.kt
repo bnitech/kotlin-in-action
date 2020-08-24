03bf348492cf2777e15ee6a062ebaec3d2cc8ed9
bnitech <bnitech88@gmail.com>
2020-08-25 02:21:48 +0900

    리스트 3.10 경로 파싱에 정규식 사용하기

---
+++ src/리스트 3.10 경로 파싱에 정규식 사용하기.kt
@@ -0,0 +1,8 @@
+fun parsePath(path: String) {
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(path)
+    if (matchResult != null) {
+        val (directory, filename, extension) = matchResult.destructured
+        println("Dir: $directory, name: $filename, ext: $extension")
+    }
+}
--- src/리스트 3.9 String 확장 함수를 사용해 경로 파싱하기.kt
+++ src/리스트 3.9 String 확장 함수를 사용해 경로 파싱하기.kt
@@ -1,7 +1,8 @@
+/*
 fun parsePath(path: String) {
     val directory = path.substringBeforeLast("/")
     val fullName = path.substringAfterLast("/")
     val fileName = fullName.substringBeforeLast(".")
     val extension = fullName.substringAfterLast(".")
     println("Dir: $directory, name: $fileName, ext: $extension")
-}
+}*/
