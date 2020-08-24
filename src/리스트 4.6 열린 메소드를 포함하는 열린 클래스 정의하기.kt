8aab8fe5be2521221863d1cdd220a98a4a2bee7f
bnitech <bnitech88@gmail.com>
2020-08-25 04:58:22 +0900

    리스트 4.7 오버라이드 금지하기

--- src/리스트 4.6 열린 메소드를 포함하는 열린 클래스 정의하기.kt
+++ src/리스트 4.6 열린 메소드를 포함하는 열린 클래스 정의하기.kt
@@ -1,5 +1,6 @@
+/*
 open class RichButton : Clickable {
     fun disable() {}
     open fun animate() {}
     override fun click() {}
-}
+}*/
---
+++ src/리스트 4.7 오버라이드 금지하기.kt
@@ -0,0 +1,3 @@
+open class RichButton : Clickable {
+    final override fun click() {}
+}
