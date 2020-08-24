ebc24d400b965cc9c26495827e800e69cd095b1f
bnitech <bnitech88@gmail.com>
2020-08-25 02:23:22 +0900

    리스트 3.13 로컬 함수에서 바낕 함수의 파라미터 접근하기

--- src/리스트 3.13 로컬 함수에서 바낕 함수의 파라미터 접근하기.kt
+++ src/리스트 3.13 로컬 함수에서 바낕 함수의 파라미터 접근하기.kt
@@ -1,16 +1,14 @@
-/*
 class User(val id: Int, val name: String, val address: String)
 
 fun saveUser(user: User) {
-
-    fun validate(user: User, value: String, fieldName: String) {
+    fun validate(value: String, fieldName: String) {
         if (value.isEmpty()) {
             throw IllegalArgumentException("Can't save user ${user.id}: empty ${fieldName}")
         }
     }
 
-    validate(user, user.name, "Name")
-    validate(user, user.address, "Address")
+    validate(user.name, "Name")
+    validate(user.address, "Address")
 
     // user를 데이터베이스에 저장한다.
-}*/
+}
